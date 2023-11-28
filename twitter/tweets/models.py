6076c6bc488ffd852603c15ca1ae596d1fc186f9
Paul Williams <paulwilliams@Pauls-Mac-mini.local>
2023-11-28 15:54:14 -0500

    serailized tweet model;

--- twitter/tweets/models.py
+++ twitter/tweets/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+import random
 
 # Create your models here.
 class Tweet(models.Model):
     content=models.CharField(max_length=280)
     image=models.FileField(upload_to='images/',blank=True,null=True)
+    # likes=models.IntegerField()
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "content":self.content,
+            "likes":random.randint(0,12345)
+        }
--- twitter/tweets/views.py
+++ twitter/tweets/views.py
@@ -26,7 +26,7 @@ def tweet_create_view(request,*args,**kwargs):
         obj = form.save(commit=False)
         obj.save()
         if is_ajax:
-            return JsonResponse({},status=201) #201 is for created items
+            return JsonResponse(obj.serialize(),status=201) #201 is for created items
         if next_url != None and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
